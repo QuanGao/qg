4e61520653fd0b7b5445099b98bd653be775c33a
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-05-15 22:35:48 -0500

    home page first draft done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,19 +2,17 @@ import React from 'react'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import "./Home.css";
 
-
 const { Header, Content, Footer} = Layout;
 class Home extends React.Component {
     render() {
         return (
             <Layout className="home">
-                {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
                 <Content style={{ margin: '0 16px' }}>
-                    <div className="bio" style={{left: this.props.collapsed?"360px":"320px", width:this.props.collapsed?"450px":"400px"}} >
+                    <div className="bio" style={{left: this.props.collapsed?"340px":"300px", width:this.props.collapsed?"450px":"400px"}} >
                         <h1> About me </h1>
                         <p>Hello there! I'm Quan, a full stack web developer and avid learner. 
-                        I am pretty comfortable throughout the MERN stack. Outside of MERN, 
-                        I have also built projects with mySQL and firebase.</p>
+                        I am pretty comfortable throughout the <strong> MERN </strong>stack. Outside of MERN, 
+                        I have also built projects with <strong> mySQL </strong> and <strong>firebase </strong>.</p>
                         <p>Writing clean code and designing optimal solutions to challenging 
                         problems give me great satisfaction. For me, spending time solving algorithm 
                         puzzles is a total treat. I am very excited to embark on a new journey into software/web development. 
