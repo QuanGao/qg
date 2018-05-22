2306c0ba9ee5fa22d46ad4e5f855e9347ad14eaf
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-05-21 23:47:10 -0500

    add treemap

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -2,45 +2,79 @@ import React from 'react'
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import "./Skills.css";
 
+import data from "./data.json";
+import {Treemap} from "recharts"
 
 const { Header, Content, Footer} = Layout;
-class Skills extends React.Component {
-    state = {
-      collapsed: false,
-    };
 
-    render() {
-        return (
-            <Layout className="content-wrapper">
-                <Header style={{ background: '#fff', padding: 0 }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                    Skills
-                    </div> 
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>
-                    Quan Gao ©2018 Web development Portfolio
-                </Footer> 
-            </Layout>
-        )
-    }
-}
+const COLORS = ['#8889DD', '#9597E4', '#8DC77B', '#A5D297', '#E2CF45', '#F8C12D'];
 
-export default Skills;
+class CustomizedContent extends React.Component{
+  render() {
+    const { root, depth, x, y, width, height, index, payload, colors, rank, name } = this.props;
 
-                        {/* <Menu.Item key="11">
-                            <a href="https://github.com/QuanGao">
-                                <Icon type="github" />
-                                <span>Github</span>
-                            </a>
-                        </Menu.Item>
-                        <Menu.Item key="12">
-                            <a href="https://www.linkedin.com/in/quan-gao-code">
-                                <Icon type="linkedin" />
-                                <span>Linkedin</span>
-                            </a>
-                        </Menu.Item> */}
+    return (
+      <g>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          style={{
+            fill: depth < 2 ? colors[Math.floor(index / root.children.length * 6)] : 'none',
+            stroke: '#fff',
+            strokeWidth: 2 / (depth + 1e-10),
+            strokeOpacity: 1 / (depth + 1e-10),
+          }}
+        />
+        {
+          depth === 1 ?
+          <text
+            x={x + width / 2}
+            y={y + height / 2 + 7}
+            textAnchor="middle"
+            fill="#fff"
+            fontSize={14}
+          >
+            {name}
+          </text>
+          : null
+        }
+        {
+          depth === 1 ?
+          <text
+            x={x + 4}
+            y={y + 18}
+            fill="#fff"
+            fontSize={16}
+            fillOpacity={0.9}
+          >
+            {index + 1}
+          </text>
+          : null
+        }
+      </g>
+    );
+  }
+};
+
+class SimpleTreemap extends React.Component{
+	render () {
+  	return (
+    	<Treemap
+      	// width={400}
+        // height={200}
+        width={800}
+        height={400}
+        data={data}
+        dataKey="size"
+        ratio={4/3}
+        stroke="#fff"
+        fill="#8884d8"
+        content={<CustomizedContent colors={COLORS}/>}
+      />
+    );
+  }
+};
+
+export default SimpleTreemap 
