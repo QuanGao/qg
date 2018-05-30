22041cc671429a8defdaf379d2922ec43e62eb93
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-05-30 00:27:08 -0500

    add function to save note

--- client/src/components/CommentBtn/CommentBtn.js
+++ client/src/components/CommentBtn/CommentBtn.js
@@ -8,48 +8,42 @@ const {TextArea} = Input
 
 
 class CommentBtn extends React.Component { 
-    // constructor(props) {
-    //     super(props)
-    // }
-    // handleClicComments = () => {
-    //     const notes = this.props.notes
-
-    // }
-    // handleSubmitComment = (projectId,content) => {
-    //     API.saveNote(projectId,content).then(
-    //         response=>console.log(response)
-    //     )
-    // }
-    // render () {
-    //     return (
-    //         <div>
-    //         <IconText onclick={this.handleClicComments} type="message" text={this.props.notes.length} />
-    //         <CommentModal/>
-    //         </div>
-    //     )
-    // }
     state = {
-        ModalText: this.props.notes[0],
         visible: false,
-        confirmLoading: false,
+        showCommentInput: false,
+        commentInput: ""
       }
+
+      handleCommentInput = e => {
+        const value = e.target.value;
+        const name =  e.target.name;
+        this.setState({
+            [name]:value
+        }, ()=>console.log(this.state))
+      }
+      handleAddCommentsBtn = () => {
+        this.setState({
+            showCommentInput: true
+          });
+        }
       showModal = () => {
         this.setState({
           visible: true,
+          showCommentInput:false
         });
       }
       handleOk = () => {
-        this.setState({
-          ModalText: 'The modal will be closed after two seconds',
-          confirmLoading: true,
-        });
-        setTimeout(() => {
-          this.setState({
-            visible: false,
-            confirmLoading: false,
-          });
-        }, 2000);
-      }
+        API.saveNote(this.props.projectId, {content:this.state.commentInput})
+            .then(response => {
+                if(!response.data.errors){
+                    this.setState({
+                        visible: false
+                    })
+                }else{
+                    console.log(`save comment error message ${response.data.errors.content.message}`)
+                }
+            })
+        }
       handleCancel = () => {
         console.log('Clicked cancel button');
         this.setState({
@@ -57,7 +51,7 @@ class CommentBtn extends React.Component {
         });
       }
       render() {
-        const { visible, confirmLoading, ModalText } = this.state;
+        const { visible } = this.state;
         const IconText = ({ type, text }) => (
             <span>
               <Icon type={type} style={{ marginRight: 8 }} onClick={this.showModal} />
@@ -66,20 +60,18 @@ class CommentBtn extends React.Component {
         );
         return (
           <div>
-            {/* <Button type="primary" onClick={this.showModal}>Open</Button> */}
             <IconText type="message" text={this.props.notes.length} />
-            {/* <Icon type="message" onClick={this.showModal} /> */}
             <Modal title={this.props.projectName}
               visible={visible}
               onOk={this.handleOk}
-              confirmLoading={confirmLoading}
               onCancel={this.handleCancel}
             >
-            {this.props.notes.length>0?
-                <div><h3 style={{color: "#1a8fff"}}><Icon type="plus-circle-o" /> Comment </h3><SimpleList data={this.props.notes}/></div>: 
-                <div><h3 style={{color: "#1a8fff"}}><Icon type="plus-circle-o" /> Comment </h3></div>}
+            
+            <h4 onClick={this.handleAddCommentsBtn} style={{color: "#1a8fff"}}><Icon type="plus-circle-o" /> Comment </h4>
+            {this.state.showCommentInput && <TextArea name="commentInput" onChange={this.handleCommentInput} placeholder="Any suggestion or comment is welcome" autosize />}
+
+            {this.props.notes.length>0?<SimpleList data={this.props.notes}/>: null}
                                
-                {/* <TextArea placeholder="Autosize height based on content lines" autosize /> */}
 
             </Modal>
           </div>
